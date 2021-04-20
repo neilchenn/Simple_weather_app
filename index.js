196e661be433fdeb0e31a2faa31b00dc01d26410
Neil <zooming.in@hotmail.com>
2021-04-20 23:39:08 +1000

    updates

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 document.addEventListener('DOMContentLoaded', function(){
 
-  const list = document.querySelector('#book-list ul');
-  const forms = document.forms;
-
-  // delete books
+  const list = document.querySelector('#city-list ul');
+  
+  // delete cities (#10)
   list.addEventListener('click', (e) => {
     if(e.target.className == 'delete'){
       const li = e.target.parentElement;
@@ -11,70 +10,130 @@ document.addEventListener('DOMContentLoaded', function(){
     }
   });
 
-  // add books
-  const addForm = forms['add-book'];
+  const forms = document.forms;
+
+  // add cities (#10)
+  const addForm = forms['add-city'];
   addForm.addEventListener('submit', function(e){
     e.preventDefault();
 
-    // create elements
+    // grabbing the value from the 'add-city' form
     const value = addForm.querySelector('input[type="text"]').value;
+
+    // create elements
     const li = document.createElement('li');
-    const bookName = document.createElement('span');
+    const cityName = document.createElement('span');
     const deleteBtn = document.createElement('span');
 
     // add text content
-    bookName.textContent = value;
+    cityName.textContent = value;
     deleteBtn.textContent = 'delete';
 
-    // add classes
-    bookName.classList.add('name');
+    // add classes (used .classList instead of .className as I can add multiple classes this way)
+    // the input in the bracket for .add() is the class name
+    cityName.classList.add('name');
     deleteBtn.classList.add('delete');
 
-    // append to DOM
-    li.appendChild(bookName);
+    // append cityName and deleteBtn to li element created
+    li.appendChild(cityName);
     li.appendChild(deleteBtn);
+
+    // append li element to the city-list under ul element (#12)
     list.appendChild(li);
   });
 
-  // hide books
+  // hide cities
   const hideBox = document.querySelector('#hide');
   hideBox.addEventListener('change', function(e){
+
+    // use the .checked property, if the box is checked, returns true, we want the list to display hidden
+    // .display = "none" will hide it; .display = "initial" or "block" will show it
     if(hideBox.checked){
       list.style.display = "none";
     } else {
-      list.style.display = "initial";
-    }
+      list.style.display = "block";
+    } 
   });
 
-  // filter books
-  const searchBar = forms['search-books'].querySelector('input');
+  // filter cities (#16)
+  const searchBar = document.forms['search-cities'].querySelector('input');
   searchBar.addEventListener('keyup', (e) => {
     const term = e.target.value.toLowerCase();
-    const books = list.getElementsByTagName('li');
-    Array.from(books).forEach((book) => {
-      const title = book.firstElementChild.textContent;
+    const cities = list.getElementsByTagName('li');
+
+    // turn the selection into an array
+    Array.from(cities).forEach((city) => {
+      const title = city.firstElementChild.textContent;
       if(title.toLowerCase().indexOf(e.target.value) != -1){
-        book.style.display = 'block';
+        city.style.display = 'block';
       } else {
-        book.style.display = 'none';
+        city.style.display = 'none';
       }
     });
   });
 
-  // tabbed content
-  const tabs = document.querySelector('.tabs');
-  const panels = document.querySelectorAll('.panel');
-  tabs.addEventListener('click', (e) => {
-    if(e.target.tagName == 'LI'){
-      const targetPanel = document.querySelector(e.target.dataset.target);
-      Array.from(panels).forEach((panel) => {
-        if(panel == targetPanel){
-          panel.classList.add('active');
-        }else{
-          panel.classList.remove('active');
-        }
-      });
-    }
-  });
 
 })
+
+// add API data
+const desc = document.getElementsByClassName('desc');
+const temp = document.getElementsByClassName('temp');
+
+fetch('https://api.openweathermap.org/data/2.5/weather?q=sydney&appid=b9a760e0f3fdbfa257d1f712d0767f67&units=metric')
+.then(response => response.json())
+.then(data => {
+  let descValue = data['weather'][0]['description'];
+  let tempValue = data['main']['temp'];
+
+  desc.textContent = descValue;
+  temp.textContent = tempValue;
+})
+
+.catch(() => {
+  msg.textContent = "Please search for a valid city 😩";
+});
+
+
+/*ajax here
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      const { main, name, sys, weather } = data;
+      const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${
+        weather[0]["icon"]
+      }.svg`;
+
+      const li = document.createElement("li");
+      li.classList.add("city");
+      const markup = `
+        <h2 class="city-name" data-name="${name},${sys.country}">
+          <span>${name}</span>
+          <sup>${sys.country}</sup>
+        </h2>
+        <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup></div>
+        <figure>
+          <img class="city-icon" src="${icon}" alt="${
+        weather[0]["description"]
+      }">
+          <figcaption>${weather[0]["description"]}</figcaption>
+        </figure>
+      `;
+      li.innerHTML = markup;
+      list.appendChild(li);
+    })
+    .catch(() => {
+      msg.textContent = "Please search for a valid city 😩";
+    });
+
+  msg.textContent = "";
+  form.reset();
+  input.focus();
+});
+
+*/
+
+/* fetch ('url')
+.then(response => response.json())
+.then(data => console.log(data)) */
